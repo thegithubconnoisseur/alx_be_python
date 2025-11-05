428a4f1dd8a1bb80a7ec38b2dafe34fd17c0ad1c
Joshua Akuma <joshuakuma.ja@gmail.com>
2025-11-05 12:49:48 +0100

    redo

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10 
+number2 = 5
+
+operations = "Addition", "Subtraction", "Multiplication"
+
+result = number1 + number2, number1 - number2, number1 * number2
+
+print(f"{operations[0]} of {number1} and {number2} is {result[0]}")
+print(f"{operations[1]} of {number1} and {number2} is {result[1]}")
+print(f"{operations[2]} of {number1} and {number2} is {result[2]}")
