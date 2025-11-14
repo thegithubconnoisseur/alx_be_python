2587951858b7c5684b7a95851301e6139d301e19
Joshua Akuma <joshuakuma.ja@gmail.com>
2025-11-14 15:48:37 +0100

    made changes

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,15 +4,15 @@ while True:
 
     weather = input("What's the weather like today? (sunny/rainy/cold): ")
     
-    if weather.lower() == "sunny":
+    if weather == "sunny":
         print("Wear a t-shirt and sunglasses.")
         break
         
-    elif weather.lower() == "rainy":
+    elif weather == "rainy":
         print("Don't forget your umbrella and a raincoat.")
         break
         
-    elif weather.lower() == "cold":
+    elif weather== "cold":
         print("Make sure to wear a warm coat and a scarf.")
         break
 
